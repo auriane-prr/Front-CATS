620f6702bce42472d385038c51e3d04d86f1d570
Auriane Poirier <poirierauriane@gmail.com>
2025-01-13 13:44:43 +0100

    utilisation cache home page admin

--- shared/src/commonMain/kotlin/com/pfe/maborneapp/view/admin/AdminHomePage.kt
+++ shared/src/commonMain/kotlin/com/pfe/maborneapp/view/admin/AdminHomePage.kt
@@ -21,14 +21,19 @@ import com.pfe.maborneapp.viewmodel.factories.BorneViewModelFactory
 import com.pfe.maborneapp.viewmodel.BorneViewModel
 
 @Composable
-fun AdminHomePage(navController: NavHostController) {
+fun AdminHomePage(navController: NavHostController, carteId: String? = null) {
     val carteViewModel: CarteViewModel = viewModel(factory = CarteViewModelFactory())
     val selectedCarteImageUrl by carteViewModel.selectedCarteImageUrl.collectAsState()
+    val selectedCarteLastModified by carteViewModel.selectedCarteLastModified.collectAsState()
 
     val borneViewModel: BorneViewModel = viewModel(factory = BorneViewModelFactory())
     val etatBornes by borneViewModel.etatBornes.collectAsState()
 
     var isMenuOpen by remember { mutableStateOf(false) }
+
+    LaunchedEffect(carteId) {
+        carteViewModel.fetchCarteDetails(carteId)
+    }
 
     Scaffold(
         containerColor = MaterialTheme.colorScheme.background,
@@ -72,6 +77,7 @@ fun AdminHomePage(navController: NavHostController) {
                     // Affichage de l'image
                     NetworkImage(
                         imageUrl = selectedCarteImageUrl,
+                        lastModified = selectedCarteLastModified,
                         contentDescription = "Carte pour Admin",
                         modifier = Modifier
                             .fillMaxWidth() // Largeur maximale avec padding
